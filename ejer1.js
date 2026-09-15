00a9a251ac160bdd30306b2c0a23e5a28a3b3f2e
shirley-11 <158266662+shirley-11@users.noreply.github.com>
2026-09-15 13:25:52 +0000

    ejercicio 1 hecho!!!!!!!

--- ejer1.js
+++ ejer1.js
@@ -1,1 +1,24 @@
-console.log("hola mundo")
+class Punto {
+
+    constructor (x, y){
+        this.x = x;
+        this.y = y;
+    }
+
+    suma (p1) {
+        let nuevaequis = p1.x + this.x;
+        let nuevay = p1.y + this.y;
+        return new Punto(nuevaequis, nuevay);
+    }
+    //console.log(new Punto(1,2).suma(new Punto(2,1)))
+
+    static suma (p1, p2){
+        let nuevaequis= p1.x + p2.x;
+        let nuevay = p1.y + p2.y;
+        return new Punto(nuevaequis, nuevay);
+    }
+    
+}
+
+console.log(Punto.suma(new Punto(1, 2), new Punto(2, 1)));
+
