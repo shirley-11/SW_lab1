44ea8bc75a9199bab9bd96ad0b0f9ed9fcd4f147
shirley-11 <158266662+shirley-11@users.noreply.github.com>
2026-09-16 15:03:16 +0000

    ej9 hecho

--- ejer9.js
+++ ejer9.js
@@ -1,5 +1,5 @@
 const redes = ["Facebook", "Twitter", "Instagram", "TikTok",
 "YouTube"];
 
-let minus= redes.forEach(red => red.toLowerCase()).filter(minus => minus.length>7);
-console.log(minus);
+let minus= redes.map(red => red.toLowerCase()).filter(minus => minus.length>7)
+.forEach(buenos => console.log(buenos));
