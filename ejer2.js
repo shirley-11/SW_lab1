d322b5517f0bece3eaaa52e9d58905e771547f38
shirley-11 <158266662+shirley-11@users.noreply.github.com>
2026-09-16 08:58:49 +0000

    haciendo ejer2

---
+++ ejer2.js
@@ -0,0 +1,25 @@
+class Locutor {
+    constructor (nombre, verbo = "dice"){
+        this.nombre = nombre;
+        this.verbo = verbo;
+    }
+    dice (texto) {
+        console.log(this.nombre + " " + this.verbo + " '" + texto + "'");
+
+    }
+}
+
+class Feriante extends Locutor {
+    constructor (nombre) {
+        super(nombre, "grita");
+    }
+
+    dice (texto) {
+        let mayus = texto.toUpperCase();
+        console.log(this.nombre + " " + this.verbo + " '" + texto + "'");
+
+    }
+}
+
+const fer1 = new Feriante ("Mr. Crecepelo");
+fer1.dice("Me lo quitan de las manos");
